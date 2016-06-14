cc4b8386ec7215b96dceb7f30aa09aa7dd65cc08
baleian <baleian90@gmail.com>
2016-06-14 20:04:45 +0900

    #2 Make package.json, gruntfile.js

---
+++ Gruntfile.js
@@ -0,0 +1,40 @@
+module.exports = function (grunt) {
+	var coreJsFiles = [
+		// Core
+		'src/this-play.core.js',
+
+		// models
+		'src/models/this-play.models.array.js',
+
+		// controllers
+		'src/controllers/this-play.controllers.array.js'
+	]
+
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		jshint: {
+			all: coreJsFiles
+		},
+		concat: {
+			basic: {
+				src: coreJsFiles,
+				dest: 'build/this-play.js'
+			}
+		},
+		uglify: {
+			options: {
+				banner: '/* <%= grunt.template.today("yyyy-mm-dd") %> /'
+			},
+			build: {
+				src: 'build/this-play.js',
+				dest: 'build/this-play.min.js'
+			}			
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+
+	grunt.registerTask('default', ['jshint', 'concat', 'uglify']);
+}
