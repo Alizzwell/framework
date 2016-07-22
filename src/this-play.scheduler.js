d2509559632b94c0b59eba6e0ec3e8c14bfeabed
baleian <baleian90@gmail.com>
2016-07-22 11:12:32 +0900

    step & event listenr test

--- src/this-play.scheduler.js
+++ src/this-play.scheduler.js
@@ -5,35 +5,77 @@
 		throw 'this_play is not declared';
 	}
 
-	var Scheduler = function (jsonData) {
-		var that = this;
-		var step = {
+	const Scheduler = function (jsonData) {
+		var _that = this;
+		var _step = {
 			current: 1,
 			max: jsonData.steps.length
 		};
 
+		this.events = [];
+
 		this.getStep = function () {
-			return step;
+			return _step;
 		};
 
 		this.getLine = function () {
-			return jsonData.steps[step.current - 1].line;
+			return jsonData.steps[_step.current - 1].line;
 		};
 
-		this.getTarget = function (name) {
+		this.getTarget = function (name, step) {
+			var step = step || _step.current;
+			
+			if (step < 1 || step > _step.max) {
+				return undefiend;
+			}
+
 			var target = jsonData.targets[name];
-			target.data = jsonData.steps[step.current - 1].status[name];
+			target.data = jsonData.steps[step - 1].status[name];
 			return target;
 		};
 
-		this.getTargets = function () {
+		this.getTargets = function (step) {
 			var targets = {};
 			for (var name in jsonData.targets) {
-				targets[name] = that.getTarget(name);
+				targets[name] = _that.getTarget(name, step);
 			}
-			return targets;
-		}
+			return JSON.parse(JSON.stringify(targets));
+		};
+
+		this.step = function () {
+			if (_step.current >= _step.max) {
+				_step.current = _step.max;
+				return;
+			}
+
+			_step.current++;
+
+			if (this.events.step) {
+				this.events.step();
+			}
+
+			if (this.events.change) {
+				var before = _that.getTargets(_step.current - 1);
+				var after = _that.getTargets(_step.current);
+				var info = {};
+				for (var name in jsonData.targets) {
+					if (before[name].data.toString() != 
+						after[name].data.toString()) {
+						info[name] = {
+							before: before[name],
+							after: after[name]
+						};
+					}
+				}
+				this.events.change(info);
+			}
+		};
 	};
+
+	Scheduler.prototype.on = function (event, callback) {
+		this.events[event] = callback;
+	};
+
 
 	this_play.Scheduler = Scheduler;
 
--- test/unit.scheduler.js
+++ test/unit.scheduler.js
@@ -7,9 +7,9 @@ const jsonData = {
   	{ "line": 12, "status": { "A": [ 0, 0, 0, 0 ], "B": 0 } },
     { "line": 19, "status": { "A": [ 4, 3, 2, 1 ], "B": 0 } },
     { "line": 19, "status": { "A": [ 3, 4, 2, 1 ], "B": 1 } },
-    { "line": 19, "status": { "A": [ 2, 4, 3, 1 ], "B": 2 } },
-    { "line": 19, "status": { "A": [ 4, 2, 3, 1 ], "B": 3 } },
-    { "line": 23, "status": { "A": [ 4, 3, 2, 1 ], "B": 4 } }
+    { "line": 19, "status": { "A": [ 3, 2, 4, 1 ], "B": 2 } },
+    { "line": 19, "status": { "A": [ 3, 2, 1, 4 ], "B": 3 } },
+    { "line": 23, "status": { "A": [ 2, 3, 1, 4 ], "B": 4 } }
   ]
 };
 
@@ -37,6 +37,85 @@ QUnit.module('unit.scheduler.js', function (hooks) {
 		});
 	});
 	
+	QUnit.test("scheduler step test", function (assert) {
+		const scheduler = new this_play.Scheduler(jsonData);
+
+		assert.equal(scheduler.getStep().current, 1);
+
+		scheduler.step();
+		assert.equal(scheduler.getStep().current, 2);
+		assert.equal(scheduler.getLine(), 19);
+		assert.deepEqual(scheduler.getTarget('A').data, [4, 3, 2, 1]);
+		assert.equal(scheduler.getTarget('B').data, 0);
+
+		scheduler.step();
+		assert.equal(scheduler.getStep().current, 3);
+		assert.equal(scheduler.getLine(), 19);
+		assert.deepEqual(scheduler.getTarget('A').data, [3, 4, 2, 1]);
+		assert.equal(scheduler.getTarget('B').data, 1);
+
+		scheduler.step();
+		assert.equal(scheduler.getStep().current, 4);
+		assert.equal(scheduler.getLine(), 19);
+		assert.deepEqual(scheduler.getTarget('A').data, [3, 2, 4, 1]);
+		assert.equal(scheduler.getTarget('B').data, 2);
+
+		scheduler.step();
+		assert.equal(scheduler.getStep().current, 5);
+		assert.equal(scheduler.getLine(), 19);
+		assert.deepEqual(scheduler.getTarget('A').data, [3, 2, 1, 4]);
+		assert.equal(scheduler.getTarget('B').data, 3);
+
+		scheduler.step();
+		assert.equal(scheduler.getStep().current, 6);
+		assert.equal(scheduler.getLine(), 23);
+		assert.deepEqual(scheduler.getTarget('A').data, [2, 3, 1, 4]);
+		assert.equal(scheduler.getTarget('B').data, 4);
+
+		scheduler.step();
+		assert.equal(scheduler.getStep().current, 6);
+	});
+
+	QUnit.test("scheduler step event listener test", function (assert) {
+		const scheduler = new this_play.Scheduler(jsonData);
+
+		assert.expect(10);
+
+		var i = 0;
+
+		scheduler.on('step', function () {
+			var targets = scheduler.getTargets();
+			assert.deepEqual(targets['A'].data, jsonData.steps[i].status['A']);
+			assert.deepEqual(targets['B'].data, jsonData.steps[i].status['B']);
+		});
+
+		while (i++ <= 100) {
+			scheduler.step();
+		}
+	});
+
+	QUnit.test("scheduler change event listener test", function (assert) {
+		const scheduler = new this_play.Scheduler(jsonData);
+
+		assert.expect(18);
+
+		var i = 0;
+
+		scheduler.on('change', function (info) {
+			for (var name in info) {
+				assert.deepEqual(
+					info[name].before.data, jsonData.steps[i - 1].status[name],
+					`name: ${name}, before: ${info[name].before.data}`);
+				assert.deepEqual(
+					info[name].after.data, jsonData.steps[i].status[name],
+					`name: ${name}, after: ${info[name].after.data}`);
+			}	
+		});
+
+		while (i++ <= 100) {
+			scheduler.step();
+		}
+	});
 
 });
 
